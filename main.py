b809fad595d4c374bb6e95bd8f67c0221049f56a
Anusha-292 <110899311+Anusha-292@users.noreply.github.com>
2025-04-26 17:50:51 +0530

    Update main.py
    test gcp cloud shell

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 def hello():
     """Return a friendly HTTP greeting."""
     print("I am inside hello world")
-    return 'Hello World! Hope everything is going good'
+    return 'Hello World! Everything will be fine one day'
 
 @app.route('/echo/<name>')
 def echo(name):
